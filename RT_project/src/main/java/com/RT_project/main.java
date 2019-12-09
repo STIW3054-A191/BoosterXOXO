6cdabef5ca6179910bca9aa60636870a89d0f59d
AhmedBawazir2020 <55338676+AhmedBawazir2020@users.noreply.github.com>
2019-12-10 03:50:30 +0800

    Add files via upload

---
+++ RT_project/src/main/java/com/RT_project/main.java
@@ -0,0 +1,14 @@
+package com.RT_project;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AssignS A = new AssignS();
+        A.app();
+	}
+
+}
