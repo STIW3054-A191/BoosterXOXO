393b6cd8de0b6bd523cdd68be5c7343ed4aa7cdc
Faris Shukri <37300366+farisleh@users.noreply.github.com>
2019-12-26 20:01:16 +0800

    Repo link

---
+++ src/main/java/com/realtime/stiw3054/RepoLink.java
@@ -0,0 +1,40 @@
+package com.realtime.stiw3054;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepoLink {
+
+    public List<String> showList() throws IOException {
+
+
+        final String link = "https://github.com/STIW3054-A191/Assignments/issues/1";
+        final Document document = Jsoup.connect(link).get();
+        Elements table = document.select("td.d-block.comment-body.markdown-body.js-comment-body");
+        List<String> listRepo=new ArrayList<String>();
+
+
+        for (Element listLink : table) {
+            String url = listLink.getElementsByTag("a").attr("abs:href");
+            System.out.println(url);
+            listRepo.add(url);
+        }
+
+        return listRepo;
+    }
+
+    }
+
+
+
+
+
+
+
+
