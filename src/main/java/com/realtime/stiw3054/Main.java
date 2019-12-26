4a7b8e6f56c5fa5c49d9972b9788164ca1b5cdda
Faris Shukri <37300366+farisleh@users.noreply.github.com>
2019-12-26 19:59:44 +0800

    Update Main.java

--- src/main/java/com/realtime/stiw3054/Main.java
+++ src/main/java/com/realtime/stiw3054/Main.java
@@ -6,12 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        System.out.println("____________________________________________________________________");
-        System.out.printf("| %-5s| %-17s| %-50s\n","No.","Matric","Name");
-        System.out.println("____________________________________________________________________");
-        StudentList list = new StudentList();
-        list.showList();
-        System.out.println(" ");
+        RepoLink link = new RepoLink();
+        link.showList();
     }
 
 }
