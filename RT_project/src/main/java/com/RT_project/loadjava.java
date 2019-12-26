a3f7324386bcbb273df6c110ccb9c359f11593fa
AhmedBawazir2020 <55338676+AhmedBawazir2020@users.noreply.github.com>
2019-12-26 17:52:08 +0800

    Update loadjava.java

--- RT_project/src/main/java/com/RT_project/loadjava.java
+++ RT_project/src/main/java/com/RT_project/loadjava.java
@@ -7,10 +7,11 @@ import static com.RT_project.AssignS.javafile;
 		 try
 	        {  
 	         // We are running "dir" and "ping" commands and  "mkdir" command to create nwe flie name 'repos'  command on cmd 
-		 Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && ping localhost\"\"&&mkdir repos \""); 
-	        	for(int i=0;i<javafile.length;i++) {
-	         Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && ping localhost\"\" &&git clone "+javafile[i]+".git \"");
-	         sleep(5000);
+		     Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && ping localhost\"\"&&mkdir repos \""); 
+			 sleep(1000);
+	             for(int i=0;i<javafile.length;i++) {
+	        	 Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && ping localhost\"\" &&git clone "+javafile[i]+".git \"");
+	       		  sleep(5000);
 	        	}
 	        } 
 	        catch (Exception e) 
