9efd76fcf1a30315752d8df98e75e299042559a5
AhmedBawazir2020 <55338676+AhmedBawazir2020@users.noreply.github.com>
2019-12-26 22:11:37 +0800

    Update main.java

--- RT_project/src/main/java/com/RT_project/main.java
+++ RT_project/src/main/java/com/RT_project/main.java
@@ -5,12 +5,10 @@ package com.RT_project;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		AssignS A = new AssignS();
-        A.app();
-        loadjava a =new loadjava();
-        a.start();
+    	        A.app();
+      	        loadjava a =new loadjava();
+       		a.start();
 	}
 
 }
