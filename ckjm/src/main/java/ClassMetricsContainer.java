2fe8a3a972cc909517cc2c0675027815db84a2cb
Aidil Qayyum <55040500+aidqayyum@users.noreply.github.com>
2019-12-29 01:33:09 +0800

    Class Metrics Container

---
+++ ckjm/src/main/java/ClassMetricsContainer.java
@@ -0,0 +1,29 @@
+import org.apache.bcel.classfile.*;
+import java.util.*;
+import java.io.*;
+
+class ClassMetricsContainer {
+
+    private HashMap<String, ClassMetrics> m = new HashMap<String, ClassMetrics>();
+
+    public ClassMetrics getMetrics(String name) {
+        ClassMetrics cm = m.get(name);
+        if (cm == null) {
+            cm = new ClassMetrics();
+            m.put(name, cm);
+        }
+        return cm;
+    }
+
+    public void printMetrics(CkjmOutputHandler handler) {
+        Set<Map.Entry<String, ClassMetrics>> entries = m.entrySet();
+        Iterator<Map.Entry<String, ClassMetrics>> i;
+
+        for (i = entries.iterator(); i.hasNext(); ) {
+            Map.Entry<String, ClassMetrics> e = i.next();
+            ClassMetrics cm = e.getValue();
+            if (cm.isVisited() && (MetricsFilter.includeAll() || cm.isPublic()))
+                handler.handleClass(e.getKey(), cm);
+        }
+    }
+}
