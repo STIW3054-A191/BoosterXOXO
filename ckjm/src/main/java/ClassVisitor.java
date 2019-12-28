bed44953e972fa74eee4a143b2de15444a4ed0c6
Aidil Qayyum <55040500+aidqayyum@users.noreply.github.com>
2019-12-28 18:10:55 +0800

    update class visitor

--- ckjm/src/main/java/ClassVisitor.java
+++ ckjm/src/main/java/ClassVisitor.java
@@ -37,4 +37,42 @@ public class ClassVisitor extends ASTVisitor {
         this.classes = new Stack<>();
         this.collectedClasses = new HashSet<>();
     }
+    @Override
+    public boolean visit(TypeDeclaration node) {
+        ITypeBinding binding = node.resolveBinding();
+
+        // there might be metrics that use it
+        // (even before a class is declared)
+        if(!classes.isEmpty()) {
+            classes.peek().classLevelMetrics.stream().map(metric -> (ASTVisitor) metric).forEach(ast -> ast.visit(node));
+            if (!classes.peek().methods.isEmpty())
+                classes.peek().methods.peek().methodLevelMetrics.stream().map(metric -> (ASTVisitor) metric).forEach(ast -> ast.visit(node));
+        }
+
+        // build a classResult based on the current type
+        // declaration we are visiting
+        String className = binding != null ? binding.getBinaryName() : node.getName().getFullyQualifiedName();
+        String type = getTypeOfTheUnit(node);
+        int modifiers = node.getModifiers();
+        classResult currentClass = new classResult(sourceFilePath, className, type, modifiers);
+        currentClass.setLoc((int) JDTUtils.countLoc(node));
+
+        // create a set of visitors, just for the current class
+        List<ClassLevelMetric> classLevelMetrics = instantiateClassLevelMetricVisitors();
+
+        // store everything in a 'class in the stack' data structure
+        ClassInTheStack classInTheStack = new ClassInTheStack();
+        classInTheStack.result = currentClass;
+        classInTheStack.classLevelMetrics = classLevelMetrics;
+
+        // push it to the stack, so we know the current class we are visiting
+        classes.push(classInTheStack);
+
+        // there might be class level metrics that use the TypeDeclaration
+        // so, let's run them
+        classes.peek().classLevelMetrics.stream().map(metric -> (ASTVisitor) metric).forEach(ast -> ast.visit(node));
+
+        return true;
+    }
+
 }
