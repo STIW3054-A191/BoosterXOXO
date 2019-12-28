f2882cdffd97da900e2e32072c12b937377e2eb1
Aidil Qayyum <55040500+aidqayyum@users.noreply.github.com>
2019-12-29 01:47:27 +0800

    Output

---
+++ ckjm/src/main/java/PrintPlainResults.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+
+public class PrintPlainResults implements CkjmOutputHandler {
+    private PrintStream p;
+
+    public PrintPlainResults (PrintStream p) {
+        this.p = p;
+    }
+
+    public void handleClass(String name, ClassMetrics c) {
+        p.println(name + " " + c.toString());
+    }
+}
