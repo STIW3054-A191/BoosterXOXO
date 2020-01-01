8d7679a964a36eccb8f706cabf49cc164bd2cbce
sufyankamal <37336667+sufyankamal@users.noreply.github.com>
2020-01-01 17:42:32 +0800

    Add files via upload

---
+++ src/main/java/Project/Excel.java
@@ -0,0 +1,37 @@
+package excel;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel implements exceldata {
+
+    public static void create(){
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        // Creating Sheets using sheet object
+        XSSFSheet sheet = workbook.createSheet(sheetName);
+
+        Row row = sheet.createRow(0);
+
+        for(int i=0; i < title.length; i++){
+            row.createCell(i).setCellValue(title[i]);
+        }
+
+        CellStyle style = workbook.createCellStyle();//Create style
+        XSSFFont font = workbook.createFont();//Create font
+        font.setBold(true);//Make font bold
+        style.setFont(font);//set it to bold
+
+        for(int i = 0; i < title.length; i++){//For each cell in the row
+            row.getCell(i).setCellStyle(style);//Set the style
+        }
+
+        //Save the Excel file
+        OutputExcel.output(workbook,sheet);
+
+    }
+}
