6e546863a7a7a7563f01df75ea81ed54c5a4f622
sufyankamal <37336667+sufyankamal@users.noreply.github.com>
2020-01-01 17:41:55 +0800

    Add files via upload

---
+++ src/main/java/Project/exceldata.java
@@ -0,0 +1,14 @@
+package excel;
+
+public interface exceldata {
+
+    // Excel File Name
+    String fileName = "CKJM.xlsx";
+
+    // Excel SheetName
+    String sheetName = "ListOfStudents";
+
+    // Excel Header
+    String[] title = {"No.", "Matric", "Name", "WMC", "DIT", "NOC", "CBO", "RFC", "LCOM"};
+
+}
